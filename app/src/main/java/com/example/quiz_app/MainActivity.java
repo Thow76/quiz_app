ec18247e7364101d1aacfe38106210e3ecd1cb33
Andrew Thow <thow1976@hotmail.co.uk>
2022-03-10 19:21:41 +0000

    5_section_10_add_fields_button_textview

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -3,8 +3,13 @@ package com.example.quiz_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button falseButton;
+    private Button trueButton;
+    private TextView questionTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
