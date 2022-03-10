4a667f651d97b369973e52d3804601fce66119db
Andrew Thow <thow1976@hotmail.co.uk>
2022-03-10 19:31:42 +0000

    7_section_10_onclicklistener_option_1

--- app/src/main/java/com/example/quiz_app/MainActivity.java
+++ app/src/main/java/com/example/quiz_app/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.quiz_app;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -19,5 +20,19 @@ public class MainActivity extends AppCompatActivity {
         falseButton = findViewById(R.id.false_button);
         trueButton = findViewById(R.id.true_button);
         questionTextView = findViewById(R.id.andswer_text_view);
+
+        falseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        trueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
